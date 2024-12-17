3ee43fa2c58492f15c6016a23ad6cc7d567674e7
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2024-12-17 11:00:56 +0100

    tried something

--- MockAPI/TestProject/StepDefinitions/ResponseObjectStepDefinition.cs
+++ MockAPI/TestProject/StepDefinitions/ResponseObjectStepDefinition.cs
@@ -1,12 +1,208 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+Given(/^The current dictionary has 10 elements in it$/, () => {
+	return true;
+});
 
-namespace TestProject.StepDefinitions
-{
-    internal class ResponseObjectStepDefinition
-    {
-    }
-}
+Then(/^The String parent is empty$/, () => {
+	return true;
+});
+
+When(/^I give an Valid JToken$/, () => {
+	return true;
+});
+
+Then(/^The JToken should be added to the dictionary$/, () => {
+	return true;
+});
+
+Given(/^The current dictionary has 10 elements in it$/, () => {
+	return true;
+});
+
+Then(/^The String parent is empty$/, () => {
+	return true;
+});
+
+When(/^I give an Invalid JToken$/, () => {
+	return true;
+});
+
+Then(/^It should throw an error$/, () => {
+	return true;
+});
+
+Given(/^The current dictionary has 0 elements in it$/, () => {
+	return true;
+});
+
+Then(/^The String parent is filled$/, () => {
+	return true;
+});
+
+When(/^I give an Valid JToken$/, () => {
+	return true;
+});
+
+Then(/^The dictionary should be empty$/, () => {
+	return true;
+});
+
+Given(/^The value is of a string$/, () => {
+	return true;
+});
+
+Then(/^It should return String as a type$/, () => {
+	return true;
+});
+
+Given(/^The value is of a User$/, () => {
+	return true;
+});
+
+Then(/^It should return User as a type$/, () => {
+	return true;
+});
+
+Given(/^The value is of a Int$/, () => {
+	return true;
+});
+
+Then(/^It should return Int as a type$/, () => {
+	return true;
+});
+
+Given(/^The JObject is empty$/, () => {
+	return true;
+});
+
+Then(/^The Path is empty$/, () => {
+	return true;
+});
+
+When(/^I give a int ObjectValue$/, () => {
+	return true;
+});
+
+Then(/^It should do nothing$/, () => {
+	return true;
+});
+
+Given(/^The JObject is empty$/, () => {
+	return true;
+});
+
+Then(/^The Path is empty$/, () => {
+	return true;
+});
+
+When(/^I give a Null ObjectValue$/, () => {
+	return true;
+});
+
+Then(/^It should add null to JObject$/, () => {
+	return true;
+});
+
+Given(/^The JObject is nested$/, () => {
+	return true;
+});
+
+Then(/^The Path is not empty$/, () => {
+	return true;
+});
+
+When(/^I give a string ObjectValue$/, () => {
+	return true;
+});
+
+Then(/^It should add string to JObject$/, () => {
+	return true;
+});
+
+Given(/^The ByteArray is encoded wrong$/, () => {
+	return true;
+});
+
+Then(/^The dictionary is empty$/, () => {
+	return true;
+});
+
+Then(/^Throw an error$/, () => {
+	return true;
+});
+
+Given(/^The ByteArray is encoded correct$/, () => {
+	return true;
+});
+
+Then(/^The dictionary is empty$/, () => {
+	return true;
+});
+
+Then(/^Gives an empty dictionary$/, () => {
+	return true;
+});
+
+Given(/^The ByteArray is encoded correct$/, () => {
+	return true;
+});
+
+Then(/^The dictionary is not empty$/, () => {
+	return true;
+});
+
+Then(/^Gives an filled dictionary$/, () => {
+	return true;
+});
+
+Given(/^The ByteArray is encoded correct$/, () => {
+	return true;
+});
+
+Then(/^The dictionary is not empty$/, () => {
+	return true;
+});
+
+Then(/^Throw an error$/, () => {
+	return true;
+});
+
+Given(/^The Json is structered correct$/, () => {
+	return true;
+});
+
+Then(/^It gives a dictionary$/, () => {
+	return true;
+});
+
+Given(/^The Json is not structered correct$/, () => {
+	return true;
+});
+
+Then(/^Throw an error$/, () => {
+	return true;
+});
+
+Given(/^The Json is structered correct$/, () => {
+	return true;
+});
+
+Then(/^It gives an empty dictionary$/, () => {
+	return true;
+});
+
+Given(/^The Dictionary is empty$/, () => {
+	return true;
+});
+
+Then(/^Gives a empty JObject$/, () => {
+	return true;
+});
+
+Given(/^The Dictionary is filled$/, () => {
+	return true;
+});
+
+Then(/^Gives a filled JObject$/, () => {
+	return true;
+});
